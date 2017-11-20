3aca2a8f6005447413325a8556b1eb626e7d5969
andtsai <33293605+andtsai@users.noreply.github.com>
2017-11-20 12:25:48 -0800

    Update random_map.gd

--- random_map.gd
+++ random_map.gd
@@ -1,30 +1,42 @@
+#square
 extends Node2D
 
-var width = 32; var height = 32; var num_grass = 300; var density = 4; var minlength = 2; var maxlength = 4
+var width = 11; var height = 11; var num_grass = 300; var density = 4; var minlength = 2; var maxlength = 4 #density cannot equal 1
 var grid = []; var dir = {0:Vector2(0,-1),1:Vector2(-1,0),2:Vector2(1,0),3:Vector2(0,1)}
 var curr = Vector2(); var step = Vector2(); var rock = 0
 var river_bed = []; var river_dir = [Vector2(1,0),Vector2(-1,0),Vector2(0,1)]
 
 func _ready():
-	self.set_pos(Vector2(800,64))
+	self.set_pos(Vector2(400,128))
 	for i in range(0,width):
 		grid.append([])
 		for j in range(0,height):
 			grid[i].append(-2)
 	for i in range(0,width):
-		for j in range(0,height,1):
-			if grid[i][j] == -2: get_node("TileMap").set_cell(i,j,0)
+		for j in range(0,height):
+			if grid[i][j] == -2: get_node("TileMap 2").set_cell(i,j,1) #random tile: 
 			if i == 0 or j == 0 or i == width-1 or j == height-1:
 				grid[i][j] = -1
-				get_node("TileMap").set_cell(i,j,3)
-		river_bed.append(Vector2(i,0))
+				get_node("TileMap 2").set_cell(i,j,0) #set border
+	for i in range(0,width):  #set door
+		for j in range(0,height):
+			for x in [((width/2)-1), (width/2), ((width/2)+1)]: #3 middle spots of row for top and bottom rows
+				for y in [0, height-1]: 
+					get_node("TileMap 2").set_cellv(Vector2 (x,y),2) #set black tile
+			for x in [0, height-1]: #3 middle spots of collumn for first and last columns
+				for y in [((width/2)-1), (width/2), ((width/2)+1)]:
+					get_node("TileMap 2").set_cellv(Vector2 (x,y),2) #set black tile
+		#river_bed.append(Vector2(i,0))
 	generate_map(minlength, maxlength, density, num_grass)
+	#generate_river_bed()
 	pass
 	
 func grass_rock(curr,dirv,len):
 	for i in range(1,len,1):
 		if grid[curr.x][curr.y] == -1: return -1
-		get_node("TileMap").set_cell(curr.x,curr.y,0)
+		get_node("TileMap 2").set_cell(curr.x,curr.y,1) # set inside
+		#rock = rock + 1
+		#if dirv.x == -1 and dirv.y == 0 and rock > 10: get_node("TileMap").set_cell(curr.x,curr.y,1); rock = 0
 		grid[curr.x][curr.y] = -1
 		curr = curr + dirv
 
@@ -42,7 +54,18 @@ func calculation_map(minlength, maxlength, density):
 	var len = rand_range(minlength, maxlength)
 	len = len + density + 1
 	grass_rock(step, dirv, len)
-	
+
 func generate_map(minlength, maxlength, density, num_grass):
 	for count in range(1,num_grass,1):
 		calculation_map(minlength, maxlength, density)
+
+#func generate_river_bed():
+	#randomize()
+	#river_bed = river_bed[randi()%river_bed.size()]
+	#while river_bed.y <= height-1:
+		#river_bed.x = min(river_bed.x,width-2); river_bed.x = max(river_bed.x, 2)
+		#if grid[river_bed.x][river_bed.y] == -3: river_bed.y = min(river_bed.y + 1,height-1)
+		#get_node("TileMap").set_cell(river_bed.x,river_bed.y,2)
+		#grid[river_bed.x][river_bed.y] = -3
+		#var river = river_dir[randi()%river_dir.size()]
+		#river_bed = river_bed + river
