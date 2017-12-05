8199a0d7cffced0e769b827e4faff0001df04670
Flan-Fira <fwz641379773@gmail.com>
2017-12-05 11:04:26 -0800

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
+var direction = Vector2()
+
 const MAX_SPEED = 400
 
 var speed = 0
-var direction = Vector2()
 var velocity = Vector2()
 
-var grid
+var target_pos = Vector2()
+var target_direction = Vector2()
+var is_moving = false
+
 var type
+var grid
+
 
 func _ready():
 	grid = get_parent()
@@ -17,20 +23,33 @@ func _ready():
 
 func _fixed_process(delta):
 	direction = Vector2()
+	speed = 0
 
 	if Input.is_action_pressed("move_up"):
 		direction.y = -1
 	elif Input.is_action_pressed("move_down"):
 		direction.y = 1
+
 	if Input.is_action_pressed("move_left"):
 		direction.x = -1
 	elif Input.is_action_pressed("move_right"):
 		direction.x = 1
 
-	if direction != Vector2():
+	if not is_moving and direction != Vector2():
+		target_direction = direction.normalized()
+		if grid.is_cell_vacant(get_pos(), direction):
+			target_pos = grid.update_child_pos(get_pos(), direction, type)
+			is_moving = true
+	elif is_moving:
 		speed = MAX_SPEED
-	else:
-		speed = 0
+		velocity = speed * target_direction * delta
 
-	velocity = speed * direction.normalized() * delta
-	move(velocity)
+		var pos = get_pos()
+		var distance_to_target = pos.distance_to(target_pos)
+		var move_distance = velocity.length()
+
+		if move_distance > distance_to_target:
+			velocity = target_direction * distance_to_target
+			is_moving = false
+
+		move(velocity)
